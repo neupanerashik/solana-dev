884ac5702296fdae3589ec981d5cb609fcfe437c
Rashik Neupane <neupanerashik1@gmail.com>
2025-07-26 23:48:30 +0545

    for sync

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,25 @@
+import WalletConnect from "@/interact-with-wallets/WalletContextProvider";
 import ReadData from "@/read-data-from-solana-network/ReadData";
 import WriteData from "@/write-data-to-solana-network/WriteData";
 import Image from "next/image";
+import styles from "../../styles/Home.module.css";
+import { AppBar } from "@/components/AppBar";
+import { PingButton } from "@/components/PingButton";
+import WalletContextProvider from "@/interact-with-wallets/WalletContextProvider";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-slate-500">
+    <main className="">
       {/* <ReadData /> */}
-      
       {/* <WriteData/> */}
+      <div className={styles.App}>
+        <WalletContextProvider>
+          <AppBar />
+          <div className={styles.AppBody}>
+            <PingButton />
+          </div>
+        </WalletContextProvider>
+      </div>
     </main>
   );
 }
